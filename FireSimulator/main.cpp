295c1354c764d2028c98d4aaacc3018b44c6666b
Роман Смирнов <pombl4.01@mail.ru>
2022-06-15 20:49:38 +0300

    implemented formulas from article for calculating constants

--- FireSimulator/main.cpp
+++ FireSimulator/main.cpp
@@ -40,12 +40,27 @@ int start_tiles[10][10] = {
 */
 
 void calculateFront(array<array<Cell, 20>, 20> tiles) {
-	int x_burn, y_burn, a, b;
-	x_burn = 10;
+	int x_burn, y_burn;
+	double a, b, c;
+	double omega = 1.7;	// area code 303
+	double wind_speed = 10;	// скорость ветра м/с
+	double LB, HB;	// константы, рассчитываемые для каждой точки горения
+	x_burn = 10;	// начальные координаты точки горения
 	y_burn = 10;
-	a = 2;
-	b = 6;
-	for (int i = 0; i < 20; i++) {
+
+	LB = 0.936 * exp(0.2566 * wind_speed) + 0.461 * exp(-0.1548 * wind_speed) - 0.397;
+	HB = (LB + sqrt(LB * LB - 1)) / (LB - sqrt(LB * LB - 1));
+
+	b = (omega / 2) * ((1 + HB) / HB);
+	a = b / LB;
+	c = b - (omega / LB);
+
+	cout << LB << "   " << HB << endl;
+	cout << a << "   " << b << "   " << c << endl;
+
+	//a = 2;
+	//b = 6;
+	/*for (int i = 0; i < 20; i++) {
 		for (int j = 0; j < 20; j++) {
 			if ((pow(i - x_burn, 2) / pow(a, 2) + pow(j - y_burn, 2) / pow(b, 2)) <= 1) {
 				tiles[i][j].state = BurnState::on_fire;
@@ -58,7 +73,7 @@ void calculateFront(array<array<Cell, 20>, 20> tiles) {
 			cout << static_cast<int>(tiles[i][j].state) << " ";
 		}
 		cout << endl;
-	}
+	}*/
 }
 
 
